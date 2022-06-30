367c9effb98655e0444718b3d7a6efa36aaa4b12
quyet12308 <quyet12308@gmail.com>
2022-06-30 16:55:34 +0700

    tao screen

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+# khởi tạo
+pygame.init()
+# creat screen
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
